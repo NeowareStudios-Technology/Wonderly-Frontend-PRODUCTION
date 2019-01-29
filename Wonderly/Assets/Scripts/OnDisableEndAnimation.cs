405aa85ce2ed08723760fe41ffee689c0d1580ca
Michael Keller <michaeldeannn@yahoo.com>
2019-01-29 15:17:09 -0500

    alpha channel UI fix and script to ensure coachmarks are disabled
    Manually changed UI textures max size to 1024 (bulk changing caused issues with alpha channel).
    Added OnDisableEndAnimation to turn off lingering coachmarks.

---
+++ Wonderly/Assets/Scripts/OnDisableEndAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//place this script on a panel and when it is deactivated, ensure all help animations are turned off;
+public class OnDisableEndAnimation : MonoBehaviour {
+	
+	public List<GameObject> coachmarks;
+
+	void OnDisable(){
+		foreach(GameObject cm in coachmarks){
+			Color alphaColorZero = Color.white;
+			alphaColorZero.a = 0.0f;
+			cm.GetComponent<Image>().color = alphaColorZero;
+			cm.GetComponent<Image>().enabled = false;
+		}
+	}
+}
